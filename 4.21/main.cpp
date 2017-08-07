6cb32bcaab00ebb6c484878caa487e548e22ee78
maqian <maqian0418@sjtu.edu.cn>
2017-08-07 11:14:41 +0800

    minor change for 4.21

--- 4.21/main.cpp
+++ 4.21/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main()
 {
+	int word;
+	vector<int> a;
+	while (cin >> word)
+	{
+		a.push_back(word);
+	}
 
+	for (auto i : a)
+	{
+		cout << i << endl;
+	}
 	return 0;
 
 }
