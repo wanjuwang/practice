4e43bea668d3efe1103b403b7bb033b201a2a369
maqian <maqian0418@sjtu.edu.cn>
2017-08-07 14:53:26 +0800

    correction for 4.21

--- 4.21/main.cpp
+++ 4.21/main.cpp
@@ -2,24 +2,20 @@
 #include <vector>
 using namespace std;
 
-//ERROR: 题目要求的是，找到a中的奇数值，将这些值本身翻倍。指的是修改a内的值，不是翻倍后push到另一个vector。
 int main()
 {
 	int word;
-	vector<int> a;
-    vector<int> b;
+	vector<int> numbers;
+    
 	while (cin >> word)
 	{
-		a.push_back(word);
+		numbers.push_back(word);
 	}
-	for (auto &i : a){
-		(i % 2) ? b.push_back(2 * i) : nullptr;
-	}
-	for (auto &j : b){
-		cout << j << " ";
+	for (auto &i : numbers){
+		i=((i % 2) ? (2 * i) : i);
+		cout << i << " ";
 	}
 	cout << endl;
 	system("pause");
 	return 0;
-
 }
