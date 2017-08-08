2c62cebc23061c0814564d1a930978f514f9692a
maqian <maqian0418@sjtu.edu.cn>
2017-08-08 21:47:07 +0800

    big lesson from 5.25 including panduan meici chongzhi

--- 5.25/main.cpp
+++ 5.25/main.cpp
@@ -3,25 +3,55 @@
 
 using namespace std;
 
-//TODO: 不要使用goto实现该题。
-//NOTE: 不要在编程中使用goto。goto能表达的逻辑都可以通过if和for, while等实现。
-int main() {
-	int a1, a2;
-	cin >> a1 >> a2;
-    begin: try
-	{
-     if (a2 == 0)
-		throw runtime_error("disivor is zero");
-	}
-	catch (runtime_error err)
-	{
-		cout << "Try again with enter two new numbers"<<endl;
-		cin >> a1 >> a2;
-		goto begin;
 
-	}
-	
+int do_while_method() {
+	int a1, a2;
+
+	bool is_error = false;
+	do {
+	   is_error = false;
+		cin >> a1 >> a2;
+		try
+		{
+			if (a2 == 0)
+				throw runtime_error("disivor is zero");
+		}
+		catch (runtime_error err)
+		{
+			is_error = true;
+			cout << "Try again with entering two new numbers" << endl;
+		}
+	} while (is_error);
+
 	cout << (a1 / a2) << endl;
 	system("pause");
 	return 0;
+}
+
+int while_method() {
+	int a1, a2;	
+	bool is_error = true;
+	while (is_error) {
+		is_error = false;
+			cin >> a1 >> a2;
+			try
+			{
+				if (a2 == 0)
+					throw runtime_error("disivor is zero");
+			}
+			catch (runtime_error err)
+			{
+				is_error = true;
+				cout << "Try again with entering two new numbers" << endl;
+			}
+	}
+	cout << (a1 / a2) << endl;
+	system("pause");
+	return 0;
+}
+
+int main()
+{
+	return do_while_method();
+	//return while_method();
 }
